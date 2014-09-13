klaseskilson/vattentornet

commit 66e4bac0fd5f8a24b02b4641ee06a74dae63c8b4

Update and destroy users as admin

app/controllers/drinks_controller.rb
@@ -66,7 +66,7 @@ class DrinksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_drink
-      @drink = Drink.find(params[:id])
+      self.resource = Drink.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.

app/assets/stylesheets/scaffolds.css.scss
@@ -1,6 +1,6 @@
 body {
   background-color: #fff;
-  color: #333;
+  // color: #333;
   font-family: verdana, arial, helvetica, sans-serif;
   font-size: 13px;
   line-height: 18px;

app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) do |u|
+    devise_parameter_sanitizer.for([:sign_up, :edit]) do |u|
       u.permit(:admin, :name, :email, :current_password, :password, :password_confirmation)
     end
   end

app/controllers/users_controller.rb
@@ -1,27 +1,34 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
   end
 
-  def show
+  def edit
   end
 
   def update
-    if params[:user][:password].blank?
-      params[:user].delete(:password)
-      params[:user].delete(:password_confirmation)
-    end
   end
 
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-
-    if @user.destroy
-        redirect_to root_url, notice: "User deleted."
-    end
+  def show
   end
+
+  # def update
+  #   if params[:user][:password].blank?
+  #     params[:user].delete(:password)
+  #     params[:user].delete(:password_confirmation)
+  #   end
+  # end
+
+  # def destroy
+  #   @user = User.find(params[:id])
+  #   @user.destroy
+
+  #   if @user.destroy
+  #       redirect_to root_url, notice: "User deleted."
+  #   end
+  # end
 
   def user_admin?
     self.admin
@@ -33,4 +40,8 @@ class UsersController < ApplicationController
     params.require(:user).permit(accessible)
   end
 
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
 end

app/views/layouts/application.html.erb
@@ -26,7 +26,6 @@
   <li><%= link_to("Drinks", drinks_path) %></li>
   <li><%= link_to("Drink types", drink_types_path) %></li>
 
-  <%= yield %>
 </div>
 <% if (defined?(notice) and !notice.blank?) or (defined?(alert) and !alert.blank?) %>
   <section class="container">

app/views/users/edit.html.erb
@@ -1,0 +1,35 @@
+<h2>Edit <%= @user.name %></h2>
+
+<%= form_for(@user, as: @user.name, url: user_path(@user), html: { method: :put }) do |f| %>
+
+  <div>
+    <%= f.label :name %><br/>
+    <%= f.text_field :name %>
+  </div>
+
+  <div><%= f.label :email %><br />
+  <%= f.email_field :email, autofocus: true %></div>
+
+  <div><%= f.label :password %> <i>(leave blank if you don't want to change it)</i><br />
+    <%= f.password_field :password, autocomplete: "off" %></div>
+
+  <div><%= f.label :password_confirmation %><br />
+    <%= f.password_field :password_confirmation, autocomplete: "off" %></div>
+
+  <div><%= f.label :current_password %> <i>(we need your current password to confirm your changes)</i><br />
+    <%= f.password_field :current_password, autocomplete: "off" %></div>
+
+  <div>
+    <%= f.label :admin %><br/>
+    <%= f.check_box :admin %>
+  </div>
+
+  <div><%= f.submit "Update" %></div>
+<% end %>
+
+
+<h3>Cancel account</h3>
+
+<p>Unhappy? <%= button_to "Cancel my account", user_path(@user), data: { confirm: "Are you sure?" }, method: :delete %></p>
+
+<%= link_to "Back", :back %>

app/views/users/index.html.erb
@@ -16,8 +16,8 @@
         <td><%= user.name %></td>
         <td><%= user.email %></td>
         <td><%= user.admin %></td>
-        <td><%= link_to 'Edit', edit_user_registration_path(user) %></td>
-        <td><%= link_to 'Destroy', registration_path(user), data: { confirm: "Are you sure?" }, method: :delete %></td>
+        <td><%= link_to 'Edit', edit_user_path(user) %></td>
+        <td><%= link_to 'Destroy', user_path(user), data: { confirm: "Are you sure?" }, method: :delete %></td>
       </tr>
     <% end %>
   </tbody>

commit a9c96815745c9b91bd4e167cd6c48dedb6c11ea9

Fix nomethod related bug
The change introduced in 66e4bac0fd5f8a24b02b4641ee06a74dae63c8b4
that changed the drinks_controller.rb file, in method set_drink
from `@drink = Drink.find(params[:id])` to
`self.resource = Drink.find(params[:id])` caused an nomethod
exception, which basically broke any view related to a single
drink. This reverts that change.

A quick google search tells me that this self.resource way of doing
things is related to devise. (Not sure that is the case though.)

app/controllers/drinks_controller.rb
@@ -66,7 +66,8 @@ class DrinksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_drink
-      self.resource = Drink.find(params[:id])
+      # self.resource = Drink.find(params[:id])
+      @drink = Drink.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.