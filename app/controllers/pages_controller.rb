80b26c06c459916171cdaee2e22dd1b6b00895fa
Klas Eskilson <klas.eskilson@gmail.com>
2014-09-14 19:32:03 +0200

    Change approach to body class (helper instead of class)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
   skip_authorization_check
   def home
     @beer_of_the_moment = Drink.order("RANDOM()").first
-    @bodyclass = "home"
+  end
+
+  def about
+  end
+
+  def contact
   end
 end
