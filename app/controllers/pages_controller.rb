d7df9d1ee04ad95473b5a8922ddaaff67d17a121
Klas Eskilson <klas.eskilson@gmail.com>
2014-09-18 22:34:00 +0200

    Make sure non-admins can view pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   before_action :set_page, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:show]
+  before_action :authenticate_user!, except: :show
   authorize_resource
 
   # GET /pages
