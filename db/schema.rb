d2535307ebbf91201c10934138eceb7575421752
Carl Englund <englund92@gmail.com>
2014-09-18 19:03:31 +0200

    Did a scaffold for news functionality. News have data members title:string body:text published:boolean and user:belongs_to  title is the header of a newsitem, body is the body of the item and published can be used to write drafts for example. user is meant to be the person logged in that's posting the newsitem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140916163616) do
+ActiveRecord::Schema.define(version: 20140918165710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,17 @@ ActiveRecord::Schema.define(version: 20140916163616) do
   end
 
   add_index "drinks", ["DrinkType_id"], name: "index_drinks_on_DrinkType_id", using: :btree
+
+  create_table "news", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "published"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "news", ["user_id"], name: "index_news_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
