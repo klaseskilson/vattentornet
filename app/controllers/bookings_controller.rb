f7da69084cca5767bf6ce726fdf48dd10c7fee80
danielronnkvist <daniel@ronnkvist.com>
2014-09-22 14:53:23 +0200

    Added variables for interval-booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,7 @@
 class BookingsController < ApplicationController
   before_action :set_booking, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :new]
+  before_action :single_or_multi_create, only: :create
   authorize_resource
 
   # GET /bookings
@@ -27,6 +28,9 @@ class BookingsController < ApplicationController
     # For each params.start_date to params.end_date
     # Put each date in params and call next line
     # Booking.create(params)
+    # d = Date.new(params[:date])
+    # d.wday, 0-6 where Sunday is zero
+    # d += 7, or the amount of days you want to add
   end
 
   # POST /bookings
@@ -78,6 +82,16 @@ class BookingsController < ApplicationController
   end
 
   private
+    def single_or_multi_create
+      if params[:interval]
+        # Send to multi_create
+        redirect_to :controller => 'bookings', :action => 'multi_create'
+      else
+        # Send to regular create
+        redirect_to :controller => 'bookings', :action => 'create'
+      end
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_booking
       @booking = Booking.find(params[:id])
@@ -85,6 +99,6 @@ class BookingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def booking_params
-      params.require(:booking).permit(:date, :description, :email, :pub, :confirmed, :user_id, :public)
+      params.require(:booking).permit(:date, :description, :email, :pub, :confirmed, :user_id, :public, :interval, :end_date, :weekdays)
     end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,4 @@
 class Booking < ActiveRecord::Base
   belongs_to :user
+  attr_accessor :interval, :end_date, :weekdays
 end
