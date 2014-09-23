7755c71abcec73ea22e4cb7bdceb4ebfaa67c6e7
Carl Englund <englund92@gmail.com>
2014-09-23 22:46:22 +0200

    News is basically done. Added styling changes to editpage and show single post page. Also removed so that it wont show a next sign on paginator if there are no more pages.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   skip_authorization_check
   def home
     @beer_of_the_moment = Drink.order("RANDOM()").first
-    @latest_post = News.paginate(:page => params[:page], :per_page => 5).order('created_at DESC')
+    @latest_post = News.where(:published => true).paginate(:page => params[:page], :per_page => 5).order('created_at DESC')
   end
 
   def about
