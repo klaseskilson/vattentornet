ec71608b730486ebbb4f293898a2bb6194233459
Klas Eskilson <klas.eskilson@gmail.com>
2014-09-29 22:06:50 +0200

    Add admin dashboard

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,15 @@
 class StaticPagesController < ApplicationController
-  skip_authorization_check
+  before_action :authenticate_user!, except: [:home]
+
   def home
     # beer of the moment
     @botm = Drink.order("RANDOM()").first
     @latest_post = News.where(:published => true).paginate(:page => params[:page], :per_page => 5).order('created_at DESC')
+  end
+
+  def admin
+    @in_stock = Drink.where(instock: true).count
+    @new_users = User.where(confirmed_at: nil).count
+    @latest_news = News.select('created_at').last
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,14 +5,15 @@ class Ability
     user ||= User.new # Guest user, not logged in
 
     if user.admin?
-        can :manage, :all # Admins can do everything
+      can :manage, :all # Admins can do everything
     else
-        can :read, Drink
-        can :read, DrinkType
-        can :read, Page
-        can :update, User do |user|
-          user.try(:user) == user
-        end
+      can :read, Drink
+      can :read, DrinkType
+      can :read, Page
+      can :read, News
+      can :update, User do |user|
+        user.try(:user) == user
+      end
     end
   end
 end
