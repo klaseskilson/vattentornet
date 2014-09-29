31cc69289845424fde081e373e3b5729315bb148
Klas Eskilson <klas.eskilson@gmail.com>
2014-09-29 16:02:27 +0200

    Add user seeds and change device email sender

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: 'klas.eskilson@gmail.com', admin: true)
+User.create(email: 'englund92@gmail.com', admin: true)
+User.create(email: 'daniel@ronnkvist.com', admin: true)
