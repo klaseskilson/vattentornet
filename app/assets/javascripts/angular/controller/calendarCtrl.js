ca07250eeef52aa37faf2635aeb9ede2f8949874
danielronnkvist <daniel@ronnkvist.com>
2014-09-30 13:21:52 +0200

    Syntax fixes and removed unecessary file

--- app/assets/javascripts/angular/controller/calendarCtrl.js
+++ app/assets/javascripts/angular/controller/calendarCtrl.js
@@ -1,14 +1,13 @@
 pubApp.controller('calendarController', function ($scope, $http) {
   $scope.month = moment();
-  var bookings
-  $http.get('/bookings.json').
-    success(function (data, status, headers, config){
+  var bookings;
+  $http.get('/bookings.json')
+    .success(function (data, status, headers, config) {
       makeArray($scope.month.daysInMonth(), data)
       bookings = data;
     });
 
   $scope.changeMonth = function(x) {
-    console.log("changeMonth")
     $scope.month = $scope.month.add(x, 'months')
     makeArray($scope.month.daysInMonth(), bookings)
   }
@@ -19,32 +18,27 @@ pubApp.controller('calendarController', function ($scope, $http) {
     var confirmed = false;
     var public = false;
     var loopDate;
-    for(var j=1; j<=i; j++)
-    {
+    for(var j=1; j<=i; j++) {
       loopDate = $scope.month.format("YYYY-MM-");
       var d = loopDate
-      if((j-1)<10)
-      {
+      if((j-1)<10) {
         loopDate += "0"+((j));
         d += "0"+(j);
       }
-      else
-      {
+      else {
         loopDate += (j);
         d += j;
       }
       var n = bookings.length != 0 ? bookings.length : 1;
-      for(var k = 0; k < n; k++)
-      {
+      for(var k = 0; k < n; k++) {
         bookings[k] = bookings[k] ? bookings[k] : {date: null}
-        if( loopDate == bookings[k].date )
-        {
+        if( loopDate == bookings[k].date ) {
           pub = bookings[k].pub;
           confirmed = bookings[k].confirmed;
           public = bookings[k].public;
         }
-        else if((moment(loopDate).weekday() == 3 || moment(loopDate).weekday() == 4) && (moment(loopDate) < moment('2014-12-15') && moment(loopDate) > moment('2014-09-01')))
-        {
+        // This line is just a hotfix until interval booking is fixed
+        else if((moment(loopDate).weekday() == 3 || moment(loopDate).weekday() == 4) && (moment(loopDate) < moment('2014-12-15') && moment(loopDate) > moment('2014-09-01'))){
           pub = true;
           confirmed = true;
           public = true;
--- app/assets/javascripts/bookings_admin.js
+++
@@ -1,5 +0,0 @@
-var form = angular.module('booking-form', []);
-
-form.controller('admin-form', function($scope, $http){
-
-})
