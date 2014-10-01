01629d5a426d1bfda62994a2a9a91797f62a0578
Klas Eskilson <klas.eskilson@gmail.com>
2014-10-01 16:49:54 +0200

    Fix access to drinks, add compression gems

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,6 +1,6 @@
 class DrinksController < ApplicationController
   before_action :set_drink, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:show]
   authorize_resource
 
   # GET /sortiment/:id/dryck
