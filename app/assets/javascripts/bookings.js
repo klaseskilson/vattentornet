6b24bcd3a166bde7b17eb629e2bb9100307ecfee
danielronnkvist <daniel@ronnkvist.com>
2015-02-11 11:30:22 +0100

    removed debugging

--- app/assets/javascripts/bookings.js
+++ app/assets/javascripts/bookings.js
@@ -1,6 +1,5 @@
 function checkDate(){
   var picked_date = Date.parse(document.getElementById('booking_date').value);
-  console.log(picked_date)
   var now = new Date();
   if(picked_date < now){
     alert("Datumet du valt är inte i framtiden!")
