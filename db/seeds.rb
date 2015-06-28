3431738c71f3fbfa60cf7466366979c9575102df
tistatos <tistatos@gmail.com>
2015-06-28 10:23:23 +0200

    added test data in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,24 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-User.create(email: 'klas.eskilson@gmail.com', admin: true)
-User.create(email: 'englund92@gmail.com', admin: true)
-User.create(email: 'daniel@ronnkvist.com', admin: true)
+#User.create(email: 'klas.eskilson@gmail.com', admin: true)
+#User.create(email: 'englund92@gmail.com', admin: true)
+#User.create(email: 'daniel@ronnkvist.com', admin: true)
+
+DrinkType.create(name: 'Lager')
+DrinkType.create(name: 'IPA')
+DrinkType.create(name: 'DIPA')
+DrinkType.create(name: 'Stout')
+
+Drink.create(name: "test-lager #1", brewery:"Toilet Brewery", country: 'Sweden',
+             percentage: 5.6, price: 10, instock: true, description: "god Lager",
+             drink_type_id: DrinkType.find_by(:name => 'Lager').id )
+Drink.create(name: "test-IPA #1", brewery:"Toilet Brewery", country: 'Sweden',
+             percentage: 5.6, price: 10, instock: true, description: "god IPA",
+             drink_type_id: DrinkType.find_by(:name => 'IPA').id )
+Drink.create(name: "test-DIPA #1", brewery:"Toilet Brewery", country: 'Sweden',
+             percentage: 5.6, price: 10, instock: true, description: "god DIPA",
+             drink_type_id: DrinkType.find_by(:name => 'DIPA').id )
+Drink.create(name: "test-Stout #1", brewery:"Toilet Brewery", country: 'Sweden',
+             percentage: 5.6, price: 10, instock: true, description: "god Stout",
+             drink_type_id: DrinkType.find_by(:name => 'Stout').id )
