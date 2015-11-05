34203fc5bf8fdf81c72b2da6fadfe21b852b7f85
Klas Eskilson <klas.eskilson@gmail.com>
2015-11-05 14:54:22 +0100

    Only validate confirmed booking dates uniqueness
    This solves the issue where the user might get confused by the text saying that
    the date is taken when it appears as free. Not entirely sure we want this,
    since it may instead create a situation where the user gets angry since their
    booking got turned down.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ActiveRecord::Base
   belongs_to :user
   attr_accessor :interval, :end_date, :weekdays
   validates_presence_of :date, :description, :email, :apartment, :name
-  validates_uniqueness_of :date
+  validates_uniqueness_of :date, conditions: -> { where(confirmed: true) }
 
   def initialize(*args,&block)
     super
