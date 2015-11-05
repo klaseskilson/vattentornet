d9ac5140bfd280a27c9b693888440fcf9499b311
Klas Eskilson <klas.eskilson@gmail.com>
2015-11-05 15:02:04 +0100

    Remove user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,2 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-
-User.create(email: 'klas.eskilson@gmail.com', admin: true)
-User.create(email: 'englund92@gmail.com', admin: true)
-User.create(email: 'daniel@ronnkvist.com', admin: true)
