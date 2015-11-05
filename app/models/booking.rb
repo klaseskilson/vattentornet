02182fbc6ba54467fe531fcce21693ad0cd0b589
Klas Eskilson <klas.eskilson@gmail.com>
2015-11-05 13:37:11 +0100

    Add booking apt nr and name migration
    Thid adds names and apt numbers to the booking model. Also add precense checks
    to model.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ActiveRecord::Base
   belongs_to :user
   attr_accessor :interval, :end_date, :weekdays
-  validates_presence_of :date, :description, :email
+  validates_presence_of :date, :description, :email, :apartment, :name
   validates_uniqueness_of :date
 
   def initialize(*args,&block)
