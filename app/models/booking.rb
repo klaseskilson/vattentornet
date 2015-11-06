624e41502a71f42c2d66a1f7bf59912a8ce0c387
Klas Eskilson <klas.eskilson@gmail.com>
2015-11-06 14:24:15 +0100

    Only require email and description if non-public

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,8 @@
 class Booking < ActiveRecord::Base
   belongs_to :user
   attr_accessor :interval, :end_date, :weekdays
-  validates_presence_of :date, :description, :email
-  validates_presence_of :apartment, :name, if: :is_not_public?
+  validates_presence_of :date
+  validates_presence_of :apartment, :name, :description, :email, if: :is_not_public?
   validates_uniqueness_of :date, conditions: -> { where(confirmed: true) }, message: 'är taget'
 
   def initialize(*args,&block)
