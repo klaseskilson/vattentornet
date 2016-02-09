233c7c72e9664e8425bfc9ea820ba38a6034ac22
Klas Eskilson <klas.eskilson@gmail.com>
2016-02-09 22:16:22 +0100

    Tweak booking validation
    This makes sure that the translations of the model's properties are properly
    translated, that fields with errors are marked and that everything is validated
    properly.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,10 @@ class Booking < ActiveRecord::Base
   belongs_to :user
   attr_accessor :interval, :end_date, :weekdays
   validates_presence_of :date
-  validates_presence_of :apartment, :name, :description, :email, if: :is_not_public?
+  # the regex ensure the email is on the format something@something.somethinglongerthan2chars
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, if: :is_not_public?
+  validates :apartment, numericality: { only_integer: true } , if: :is_not_public?
+  validates_presence_of :email, :name, :apartment, :description, if: :is_not_public?
   validates_uniqueness_of :date, conditions: -> { where(confirmed: true) }, message: 'är taget'
 
   def initialize(*args,&block)
